cc958e1b3616079e20b9deeee2a553e9ceadb9b3
Serhii Bieliakov <sergiobelya@gmail.com>
2024-03-30 00:21:12 +0200

    kbot version 1.0.2

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -44,6 +44,11 @@ to quickly create a Cobra application.`,
 
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
 			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
+			}
 			return err
 		})
 
