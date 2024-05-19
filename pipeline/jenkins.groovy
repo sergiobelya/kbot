bb3230de3ef4160f8ad63f96b3bd8152a92136e8
Serhii Bieliakov <sergiobelya@gmail.com>
2024-05-19 20:08:42 +0300

    Add stage clone to Jenkins pipeline

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,17 +1,27 @@
 pipeline {
     agent any
+
     parameters {
-
-        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
-
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows'], description: 'Pick OS')
+        choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick ARCH')
     }
+
+    environment {
+        REPO = 'https://github.com/sergiobelya/kbot'
+        BRANCH = 'main'
+    }
+
     stages {
         stage('Example') {
             steps {
                 echo "Build for platform ${params.OS}"
-
                 echo "Build for arch: ${params.ARCH}"
-
+            }
+        }
+        stage('clone') {
+            steps {
+                echo 'Clone Repository'
+                git branch: "${BRANCH}", url: "${REPO}"
             }
         }
     }
